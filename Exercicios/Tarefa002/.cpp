8c092cef507398fbd0abf573f836085ffaa95bb9
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-08-16 20:30:02 -0300

    Update .cpp

--- Exercicios/Tarefa002/.cpp
+++ Exercicios/Tarefa002/.cpp
@@ -1,10 +1,29 @@
+//#include <studio.h>
+//#include <studlib.h>
+//Int main () {
+//int N_atual, Quantidade,visitante;
+  //printf("Informe a atual quantidade de visitantes:%")
+    //scanf("%i",&N_atual);
+    //Scanf("%i",&Quantidade);
+    //scnaf("%i", &visistantes);
+  //Quantidade = N_atual + visitantes;
+  //printf("O total de visitantes é:"%i",Quantidade);
+//Return(0)
+//}
+
+#include <studio.h>
+
 Int main () {
-int N_atual;
-int Quantidade;
-  printf("Informe a atual quantidade de visitantes:")
-    scanf("%i",& N_atual);
-    Scanf("%i
+int N_atual, Quantidade,visitantes;
+  printf("Informe a atual quantidade de visitantes:%i",)
+    scanf("%i",&N_atual);
+
+  printf("Informe quantos visitantes entraram:%i",visitantes)
+    scanf("%i", &visitantes);
     
-
-
+  Quantidade = N_atual + visitantes;
+  printf("O total de visitantes é:%i",Quantidade);
+    scanf("%i",&Quantidade);
+    
+Return(0);
 }
