bedb04c484a2692122c92412d6fe89d7bc201abd
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-08-18 22:15:43 -0300

    Update b.cpp

--- Exercicios/Tarefa002/b.cpp
+++ Exercicios/Tarefa002/b.cpp
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+float receitas, despesas,saldo;
+    printf ("informe o valor da receitas:");
+        scanf ("%f",&receitas);
+    printf ("informe o valor da despesas:");
+        scanf ("%f",&despesas);
+saldo =receitas + despesas;
+    printf ("saldo atualizado:%d",saldo);
+        scanf ("%f",&saldo);
+return 0;
+}
