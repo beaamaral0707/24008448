7877522154ecc71c2e87ffaddb295a5f30068b05
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-09-06 20:38:42 -0300

    Create Lista - exercício 1.cpp

---
+++ Exercicios/Tarefa 3/Lista - exercício 1.cpp
@@ -0,0 +1,37 @@
+// A) Errado
+# include < stdio .h >
+ int main () {
+ int n = 4000000000;
+ printf ( " % d \ n " , n );
+ return 0;
+ }
+// A) Corrigido
+#include <stdio.h>
+ int main () {
+ double n = 4000000000;
+ printf ( "%.1lf\n" ,n );
+ return 0;
+}
+
+//B)
+ # include < stdio .h >
+ int main () {
+ printf ( " % d \ n " , a );
+ int a ;
+ return 0;
+}
+
+// B) Corrigido
+ # include < stdio .h >
+ int main () {
+ int a ;
+ printf ( " % d \ n " , a );
+ return 0;
+}
+
+//C)
+ # include < stdio .h >
+ int main () {
+ printf ( " % d " , 4);
+ printf ( " % d " , 2);
+ return 0;
