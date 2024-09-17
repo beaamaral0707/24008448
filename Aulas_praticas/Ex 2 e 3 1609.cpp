395c2593533a084388041ad5d20bb0932d575e8d
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-09-16 22:06:56 -0300

    Create Ex 2 e 3 1609.cpp

---
+++ Aulas_praticas/Ex 2 e 3 1609.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main()
+{
+int num;
+num=1;
+printf("\n\t Imprimir os números de 1 até 50 : \n");
+while (num<=50)
+{
+printf(" %d\n",num);
+num++;
+}
+return 0;
+}
+
+
+
+////////////////////////////////////////////////////////
+#include<stdio.h>
+int main()
+{
+int num;
+
+printf("\n\t Imprimir os números decrescente: \n");
+scanf("%d",&num);
+while (num>=0)
+{
+printf(" %d\n",num);
+num--;
+}
+return 0;
+// Exercício 2: Imprima os números na forma decrescente 50, 49, ...1
+// Exercício 3: Permita que o usuário digite o número inicial, e mostre em ordem
+//decrescente até 0.
+}
+
+//////////////////////////////////////////////////////
+#include <stdio.h>
+int main()
+{
+int num,mult = 1;
+printf("\n\t Digite o numero desejado da tabuada: \n");
+scanf("%d",&num);
+while (mult<=11)
+{
+printf(" %d\n",a);
+a = num * mult++;
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
