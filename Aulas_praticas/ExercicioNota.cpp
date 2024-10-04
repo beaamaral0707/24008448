8527111c57cd73d07aa0908d308bfbe5f7d7c0d2
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-10-04 20:53:03 -0300

    Create ExercicioNota.cpp

---
+++ Aulas_praticas/ExercicioNota.cpp
@@ -0,0 +1,17 @@
+// Exercicio Dados = 7.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+soma_dado = 7;
+
+int main () {
+int dado1,dado2;
+ for(dado1=1; dado1<=6; dado1++){
+    for(dado2=1; dado2<=6; dado2++){
+        if(dado1 + dado2 == soma_dado)
+    printf(" %d + %d = %d \n",dado1,dado2,soma_dado);
+    }
+    }
+return 0;
+    }
