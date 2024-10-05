cd3a78db2e93ef6f7a95a20ca30087960df19dc2
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-10-05 16:01:47 -0300

    Update ExercicioNota.cpp

--- Aulas_praticas/ExercicioNota.cpp
+++ Aulas_praticas/ExercicioNota.cpp
@@ -1,17 +1,39 @@
 // Exercicio Dados = 7.
-
 #include <stdio.h>
 #include <stdlib.h>
 
 soma_dado = 7;
-
 int main () {
 int dado1,dado2;
  for(dado1=1; dado1<=6; dado1++){
     for(dado2=1; dado2<=6; dado2++){
         if(dado1 + dado2 == soma_dado)
-    printf(" %d + %d = %d \n",dado1,dado2,soma_dado);
-    }
-    }
+    printf(" As possibilidades são: %d + %d = %d \n",dado1,dado2,soma_dado);
+    } }
+printf("\n Criado por Beatriz Fort Mendes do Amaral, RA:24008448");
 return 0;
     }
+
+// Soma dos dados a sua escolha
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma_dado;
+
+printf("\n Insira a soma desejada");
+int main () {
+int dado1,dado2;
+ for(dado1=1; dado1<=6; dado1++){
+    for(dado2=1; dado2<=6; dado2++){
+        if(dado1 + dado2 == soma_dado)
+    printf(" As possibilidades são: %d + %d = %d \n",dado1,dado2,soma_dado);
+    } }
+printf("\n Criado por Beatriz Fort Mendes do Amaral, RA:24008448");
+return 0;
+    }
+
+
+
+
+
+
