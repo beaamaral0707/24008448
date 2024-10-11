d23dec879841e69dc4fa81401f8a17817c64843b
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-10-11 20:44:41 -0300

    Update Exercicio Projeto.cpp

--- Aulas_praticas/Exercicio Projeto.cpp
+++ Aulas_praticas/Exercicio Projeto.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h> 
+#include <locale.h>
 
 int main (){
-char[50];
+    
 int num_modelo;
-
 printf( "\tModelos disponíveis:\t \n");
 printf("1. Fiat uno\n");
 printf("2. Ford KA\n");
@@ -12,12 +12,15 @@ printf("4. Mustang 1970\n");
 printf("5. Audi Q5\n");
 printf("Escolha o modelo desejado(1-5): ");
 scanf("%d", &num_modelo);
+printf("\n");
+
+setlocale(LC_ALL, "Portuguese");
 // Exibir o preço e a especificacao com base na escolha
-switch (escolha) {
+switch (num_modelo) {
 case 1:
 printf("Modelo: Fiat uno \n");
 printf("Preco: R$27.000,00\n");
-printf("Especificacoes: de cor branca, quatro portas, direcao hidraulica.\n");
+printf("Especificações: de cor branca, quatro portas, direcao hidraulica.\n");
 break;
 case 2:
 printf("Produto: Borracha\n");
@@ -27,8 +30,7 @@ break;
 case 3:
 printf("Produto: Apontador\n");
 printf("Preco: R$ 1.75\n");
-printf("Especificacao: Apontador de plastico com deposito, cores
-variadas.\n");
+printf("Especificacao: Apontador de plastico com deposito, cores variadas.\n");
 break;
 default:
 printf("Erro: Produto invalido.\n");
