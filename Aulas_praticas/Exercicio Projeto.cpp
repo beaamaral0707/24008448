09e796f24b84c5f8507dcac34205a211c18b14a9
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-10-11 20:36:54 -0300

    Update Exercicio Projeto.cpp

--- Aulas_praticas/Exercicio Projeto.cpp
+++ Aulas_praticas/Exercicio Projeto.cpp
@@ -4,10 +4,41 @@ int main (){
 char[50];
 int num_modelo;
 
-printf( "Escolha um dos modelos disponíveis "
+printf( "\tModelos disponíveis:\t \n");
+printf("1. Fiat uno\n");
+printf("2. Ford KA\n");
+printf("3. Porsche GT3\n");
+printf("4. Mustang 1970\n"); 
+printf("5. Audi Q5\n");
+printf("Escolha o modelo desejado(1-5): ");
+scanf("%d", &num_modelo);
+// Exibir o preço e a especificacao com base na escolha
+switch (escolha) {
+case 1:
+printf("Modelo: Fiat uno \n");
+printf("Preco: R$27.000,00\n");
+printf("Especificacoes: de cor branca, quatro portas, direcao hidraulica.\n");
+break;
+case 2:
+printf("Produto: Borracha\n");
+printf("Preco: R$ 1.00\n");
+printf("Especificacao: Borracha branca, apaga lapis sem manchas.\n");
+break;
+case 3:
+printf("Produto: Apontador\n");
+printf("Preco: R$ 1.75\n");
+printf("Especificacao: Apontador de plastico com deposito, cores
+variadas.\n");
+break;
+default:
+printf("Erro: Produto invalido.\n");
+break;
+}
+return 0;
+}
 
-
-
+//  utilizar da sintaxe de mudar o printf para Portugal
+// strcmp
 
 
 
