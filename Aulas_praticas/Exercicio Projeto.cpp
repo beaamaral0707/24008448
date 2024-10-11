ceec417349a025789e82e8007a9b61844658c179
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-10-11 20:49:47 -0300

    Update Exercicio Projeto.cpp

--- Aulas_praticas/Exercicio Projeto.cpp
+++ Aulas_praticas/Exercicio Projeto.cpp
@@ -10,7 +10,7 @@ printf("2. Ford KA\n");
 printf("3. Porsche GT3\n");
 printf("4. Mustang 1970\n"); 
 printf("5. Audi Q5\n");
-printf("Escolha o modelo desejado(1-5): ");
+printf("Escolha o Modelo desejado(1-5): ");
 scanf("%d", &num_modelo);
 printf("\n");
 
@@ -19,21 +19,32 @@ setlocale(LC_ALL, "Portuguese");
 switch (num_modelo) {
 case 1:
 printf("Modelo: Fiat uno \n");
-printf("Preco: R$27.000,00\n");
-printf("Especificações: de cor branca, quatro portas, direcao hidraulica.\n");
+printf("Preco: R$ 27.000,00\n");
+printf("Especificações: de cor branca, quatro portas, direção hidraulica.\n");
 break;
 case 2:
-printf("Produto: Borracha\n");
-printf("Preco: R$ 1.00\n");
-printf("Especificacao: Borracha branca, apaga lapis sem manchas.\n");
+printf("Modelo: Fort KA\n");
+printf("Preco: R$\n");
+printf("Especificacao: , apaga lapis sem manchas.\n");
 break;
 case 3:
-printf("Produto: Apontador\n");
-printf("Preco: R$ 1.75\n");
-printf("Especificacao: Apontador de plastico com deposito, cores variadas.\n");
+printf("Modelo: Porsche GT3\n");
+printf("Preco: R$ \n");
+printf("Especificacao: , cores variadas.\n");
 break;
+case 4:
+printf("Modelo: Mustang 1970\n");
+printf("Preco: R$ \n");
+printf("Especificacao: , cores variadas.\n");
+break;
+case 5:
+printf("Modelo: Audi Q5\n");
+printf("Preco: R$ \n");
+printf("Especificacao: , cores variadas.\n");
+break;
+    
 default:
-printf("Erro: Produto invalido.\n");
+printf("Erro: Modelo inválido.\n");
 break;
 }
 return 0;
