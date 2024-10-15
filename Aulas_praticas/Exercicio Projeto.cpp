0dc18f6a6134c819389602faef5cbd4690742307
beaamaral0707 <beatriz.fmamaral@gmail.com>
2024-10-15 13:06:06 -0300

    Update Exercicio Projeto.cpp

--- Aulas_praticas/Exercicio Projeto.cpp
+++ Aulas_praticas/Exercicio Projeto.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h> 
 #include <locale.h>
 
+float v_carro1, v_carro2,v_carro3,v_carro4,v_carro5;
+
 int main (){
+v_carro1 = 27.846;
     
-int num_modelo;
+int num_modelo, mod_pag;
 printf( "\tModelos disponíveis:\t \n");
 printf("1. Fiat uno\n");
 printf("2. Ford KA\n");
@@ -12,6 +15,14 @@ printf("4. Mustang 1970\n");
 printf("5. Audi Q5\n");
 printf("Escolha o Modelo desejado(1-5): ");
 scanf("%d", &num_modelo);
+printf("\n");
+
+printf( "\tModo de pagamento:\t \n");
+printf("1. À vista: \n");
+printf("2. Parcelamento com entrada: \n");
+printf("3. Parcelamento sem entrada:  \n");
+printf("Escolha o modo de pagamento desejado (1-3): ");
+scanf("%d", &mod_pag);
 printf("\n");
 
 setlocale(LC_ALL, "Portuguese");
@@ -47,17 +58,37 @@ default:
 printf("Erro: Modelo inválido.\n");
 break;
 }
+
+// Entrada minima sendo 20% do valor do carro
+    
+switch (mod_pag) {
+case 1:
+printf("Modo de pagamento selecionado: À vista.\n");
+break;
+case 2:
+printf("Modo de pagamento selecionado: Parcelado com valor de entrada:\n");
+break;
+case 3:
+printf("Modo de pagamento escolhido: Parcelado sem valor de entrada:\n");
+break;
+    
+default:
+printf("Erro: Modo de pagamento selecionada inválido!.\n");
+break;  
+}
+
+if ((mod_pag == 2) || (mod_pag == 3)) {
+    printf("Em caso de financiamento, o cliente deve escolher a quantidade de parcelas,dentre as opções a seguir.\n");
+    printf("Parcelamento em 12x\n");
+    printf("Parcelamento em 24x\n");
+    printf("Parcelamento em 36x\n");
+} else {
+    printf("Beatriz Amaral");
+}  
+    
 return 0;
 }
 
 //  utilizar da sintaxe de mudar o printf para Portugal
 // strcmp
 
-
-
-
-
-
-
-
-}
