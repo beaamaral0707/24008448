14212fa4cf18193682350811859c8e22614dc18d
beaamaral0707 <beatriz.fmamaral@gmail.com>
2025-09-04 08:05:33 -0300

    Update and rename 20250829.cpp to Lista_Exercicios.cpp

---
+++ AULAS EVERTON/Lista_Exercicios.cpp
@@ -0,0 +1,29 @@
+//2) Elabore um programa em Linguagem C, que pergunte o seu nome e imprima
+//na tela;*#
+
+
+
+//5) Elabore um programa em Linguagem C, que solicite o último dígito do seu
+//RA e imprima na tela se seu RA é par ou ímpar;
+
+
+
+
+
+//Elabore um programa em Linguagem C, que solicite ao utilizador a
+//temperatura em graus Celsius e a converta para Fahrenheit. A fórmula de
+//conversão é: F = (C * 9/5) + 32;
+
+include <studio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+float fahrenheit;
+int celsius;
+
+printf("Insira sua temperatuar em graus Celsius(°):");
+scanf("%lf",&fahrenheit)
+celsius=(0.556*(F-32));
+printf("a temperatura em graus celsius e:%2lf\n",celsius);
+return 0;}
