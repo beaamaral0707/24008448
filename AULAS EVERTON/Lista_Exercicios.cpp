0a940993887a4f5ee348c8995d34d2e87fb2181c
beaamaral0707 <beatriz.fmamaral@gmail.com>
2025-09-04 08:12:06 -0300

    Update Lista_Exercicios.cpp

--- AULAS EVERTON/Lista_Exercicios.cpp
+++ AULAS EVERTON/Lista_Exercicios.cpp
@@ -10,11 +10,11 @@
 
 
 
-//Elabore um programa em Linguagem C, que solicite ao utilizador a
+//8) Elabore um programa em Linguagem C, que solicite ao utilizador a
 //temperatura em graus Celsius e a converta para Fahrenheit. A fórmula de
 //conversão é: F = (C * 9/5) + 32;
 
-include <studio.h>
+
 #include <stdlib.h>
 #include <math.h>
 
@@ -22,8 +22,10 @@ int main(){
 float fahrenheit;
 int celsius;
 
-printf("Insira sua temperatuar em graus Celsius(°):");
-scanf("%lf",&fahrenheit)
-celsius=(0.556*(F-32));
-printf("a temperatura em graus celsius e:%2lf\n",celsius);
+printf("Insira sua temperatuar em graus Celsius(°):",celsius);
+
+fahrenheit=((celsius * 9/5) + 32 );
+scanf("%lf",&fahrenheit); 
+  
+printf("A temperatura inserida em Fahrenheit sera de:%2lf\n",);
 return 0;}
